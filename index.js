c96b257a1ba161f5cd1b7af6345eb9cd94b8276f
Keith Capers <K.Capers94@gmail.com>
2024-02-09 23:23:30 -0500

    insert image

--- index.js
+++ index.js
@@ -39,7 +39,28 @@ const playersArray = [
     }
     
   ] 
+
   
+  //create fetch with the db.json
+  //done
+  //create a css file 
+  //done
+  //app will be either a black or white background whichever is easier
+  //white
+  //title will be Goat Vote!
+  //done
+  //3 pictures will be there 1 of MJ, Kobe, Lebron
+  // 
+  //create a hover event so there names will appear when the mouse is over the picture
+  //create a search bar
+  //create a submit event
+  //Type in a players name there stats will appear, with a picture
+  //there will be a button to vote for them
   
+ 
+
+
+
+
 
 
