c1871870767a04de0377d204227f45634a3da51c
Keith Capers <K.Capers94@gmail.com>
2024-04-06 09:37:01 -0400

    added 3 more players

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => console.log("I GOT THIS!!!!"))
 
-fetch('http://localhost:3000/players')
-
-const playersArray = [
-    {
-      id:1,
-      name: "Michael Jordan",
-      team: "Chicago Bulls, Washington Wizards",
-      position: "Shooting Guard",
-      height: "6'6",
-      titleWins: 6,
-      careerPoints: 32292,
-      careerAvgPoints: 30.1,
-      imageurl: "https://images.app.goo.gl/MoAUKZwvxEpeFssv7"
-      
-    }, 
-    {
-    id:2,
-    name: "Kobe Bryant",
-      team: "Los Angeles Lakers",
-      position: "Shooting Guard",
-      height: "6'6",
-      titleWins: 5,
-      careerPoints: 33643,
-      careerAvgPoints: 25,
-      imageurl: "https://images.app.goo.gl/mvpbwrzEpEYuhg2q9"
-    }, 
-    {
-       id:3,
-       name: "Lebron James",
-       team: "Cleveland Cavalers, Miami Heat, Los Angeles Lakers",
-       position: "Shooting Guard",
-       height: "6'8",
-       titleWins: 4,
-       careerPoints: 39797,
-       careerAvgPoints: 27.1,
-       imageurl: "https://images.app.goo.gl/mXRf2hoGaqqwAtXd9"
-    }
+const init = () => {
+  const inputForm = document.querySelector("#nbaCard")
+  
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("#playerName").value;
     
-  ] 
+
+  fetch(`http://localhost:3000/players/${input}`)
+  .then((res) => res.json())
+  .then((data) => console.log(data)
+)});
+};
+
+
+
+document.addEventListener("DOMContentLoaded", init)
+ 
+
+
+
+//I want the user to be able to search for a player 
+    // lets make sure the submit event works
+    // how do i catch the user input 
+
+//by inputing a name and click a button to submit
+// informatioin comes up they will see the info and stats of that player
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+
 
  
 
- 
-
   
   
   
   
-    //"https://media4.s-nbcnews.com/i/streams/2012/August/120814/412208-tdy-120810-tih-jordan-orig.jpg" alt="MJ" height="285" width="300" img>
-    //<img id="Kobe" src="https://media.gettyimages.com/id/51790736/photo/kobe-bryant-action-portrait.jpg?s=594x594&w=gi&k=20&c=qo6s4_RaVzmgZoV5mvzp_n6Ehl0RMjzRW4Orutzdyaw=" alt="Kobe" height="285" width="300" img>
-    //<img id="lebron" src= "https://cdn.vox-cdn.com/thumbor/IfZUNqH7ci9nprtByj7PaOPVJlw=/0x0:600x414/1200x800/filters:focal(252x159:348x255)/cdn.vox-cdn.com/uploads/chorus_image/image/60515279/gyi0062723774.0.0.0.jpg" alt="Lebron" height="285" width="300" img>
-    //<h3>Michael Jordan</h3>
-  
+   
   
   
   
@@ -72,13 +67,7 @@ const playersArray = [
   
   
   //create fetch with the db.json
-  //done
-  //create a css file 
-  //done
   //app will be either a black or white background whichever is easier
-  //white
-  //title will be Goat Vote!
-  //done
   //3 pictures will be there 1 of MJ, Kobe, Lebron
   // done
   //create a hover event so there names will appear when the mouse is over the picture
