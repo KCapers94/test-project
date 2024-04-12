15390be7f74bdebc6ec50496354a9737e200df63
Keith Capers <K.Capers94@gmail.com>
2024-04-11 23:10:26 -0400

    added multiple p tags

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-
 const init = () => {
-  const inputForm = document.querySelector("form")
+  const inputForm = document.querySelector("form");
 
   inputForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = document.querySelector("input#search-by-name")
-    const inputValue = input.value
+    const input = document.querySelector("input#search-by-name");
+    const inputValue = input.value;
 
-  
     fetch("http://localhost:3000/players")
-    .then((resp) => resp.json())
-    .then((data) => { 
-      const typedName = input.value
-      const foundName = data.filter(item => item.name === typedName)
-      console.log(foundName)
-    })
-    
-})
+      .then((resp) => resp.json())
+      .then((data) => {
+        const filteredData = data.filter((player) => player.name === inputValue);
+        renderBballCard(filteredData);
+      });
+  });
+};
 
+document.addEventListener("DOMContentLoaded", init);
+
+function renderBballCard(players) {
+  const nbaCard = document.getElementById("nba-card");
+  nbaCard.innerHTML = ""; 
+
+  players.forEach((player) => {
+    const card = document.createElement("ul");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${player.img}">
+      <div class="content">
+        <h4>${player.name}</h4>
+        <p> Team- ${player.team}</p>
+        <p> Position- ${player.position}</p>
+        <p> Height- ${player.height}</p>
+        <p> TitleWins- ${player.titleWins}</p>
+        <p> Career Points- ${player.careerPoints}</p>
+        <p> Career Average Points- ${player.careerAvgPoints}</p>
+      </div>
+    `;
+
+    nbaCard.append(card);
+  });
 }
-  
-
-document.addEventListener("DOMContentLoaded", init)
- 
-
 
 
 //I want the user to be able to search for a player 
@@ -32,61 +48,3 @@ document.addEventListener("DOMContentLoaded", init)
 
 //by inputing a name and click a button to submit
 // informatioin comes up they will see the info and stats of that player
-
-
-  
-
-
-
-
-
-
-
-
-
-  
-
-
- 
-
-  
-  
-  
-  
-   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //create fetch with the db.json
-  //app will be either a black or white background whichever is easier
-  //3 pictures will be there 1 of MJ, Kobe, Lebron
-  // done
-  //create a hover event so there names will appear when the mouse is over the picture
-
-  //create a search bar
-  //create a submit event
-  //Type in a players name there stats will appear, with a picture
-  //there will be a button to vote for them
-  
- 
-
-
-
-
-
-
